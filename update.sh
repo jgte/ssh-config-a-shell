1ac93d5dd75ed86e180972183ab4aa0dd0e8990e
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2024-12-30 17:35:46 +0100

    added check for a-Shell environment

--- update.sh
+++ update.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+#make sure we're in the right place
+case "$(basename ${APPDIR:-empty})" in
+  "a-Shell.app")
+    echo "Detected a-Shell"
+  ;;
+  *)
+    echo "ERROR: this script only makes sense to run in the a-Shell app (https://github.com/holzschu/a-shell)"
+    exit 3
+  ;;
+esac
+
 #load relevant parameters
 ./config.sh
 
