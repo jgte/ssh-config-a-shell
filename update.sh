66086a655d4deafa23a69581c31e5d4f6cb26f27
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2024-12-30 17:00:22 +0100

    added update.sh

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#load relevant parameters
+source config
+
+if ! test -e $NORMAL_SSH_CONFIG
+then
+  echo "ERROR: cannot find ssh config file '$NORMAL_SSH_CONFIG'"
+  exit 3
+fi
+
+sed 's:identityfile ~/.ssh/:IdentityFile ~/Documents/.ssh/:Ig' $NORMAL_SSH_CONFIG > $ASHELL_SSH_CONFIG
