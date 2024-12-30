09ffee582b2f4db7715993eccab2777a584e7242
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2024-12-30 17:35:59 +0100

    renamed CONFIG to PKG_INSTALL_LIST

--- pkg-install.sh
+++ pkg-install.sh
@@ -15,14 +15,14 @@ esac
 ./config.sh
 
 #check if config file for this script exists (relative to the current dir)
-if ! test -e $CONFIG
+if ! test -e $PKG_INSTALL_LIST
 then
-  echo "Need $CONFIG file to proceed; see https://github.com/jgte/ssh-config-a-shell for more info."
+  echo "Need $PKG_INSTALL_LIST file to proceed; see https://github.com/jgte/ssh-config-a-shell for more info."
   exit 3
 fi
 
 #install relevant packages
-for i in $(cat $CONFIG)
+for i in $(cat $PKG_INSTALL_LIST)
 do
   pkg list $i | grep -q $i && echo "$i is installed" || pkg install $i
 done
