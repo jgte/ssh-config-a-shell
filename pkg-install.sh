132d4c72ea89283c54e06bdb406951ae722c97af
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2024-12-30 16:46:31 +0100

    added pkg-install.list and pkg-install.sh

---
+++ pkg-install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+CONFIG=pkg-install.list
+
+#check if config file for this script exists (relative to the current dir)
+if ! test -e $CONFIG
+then
+  echo "Need $CONFIG file to proceed; see https://github.com/jgte/ssh-config-a-shell for more info."
+  exit 3
+fi
+
+#install relevant packages
+for i in $(cat $CONFIG)
+do
+  pkg list $i | grep -q $i && echo "$i is installed" || pkg install $i
+done
