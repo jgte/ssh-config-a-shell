f9c3b46a9a827bc9ad9c158e064532e87ff4a951
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2024-12-30 17:46:08 +0100

    showing values of config parameters

--- config.sh
+++ config.sh
@@ -6,3 +6,10 @@ export SSH_ALIAS="alias ssh='ssh -F ~/Documents/.ssh/config.a-Shell'"
 export NORMAL_SSH_CONFIG=$SSH_DIR/config
 export ASHELL_SSH_CONFIG=$NORMAL_SSH_CONFIG.a-Shell
 export PKG_INSTALL_LIST=pkg-install.list
+
+echo "SSH_DIR=$SSH_DIR"
+echo "DOT_PROFILE=$DOT_PROFILE"
+echo "SSH_ALIAS=$SSH_ALIAS"
+echo "NORMAL_SSH_CONFIG=$NORMAL_SSH_CONFIG"
+echo "ASHELL_SSH_CONFIG=$ASHELL_SSH_CONFIG"
+echo "PKG_INSTALL_LIST=$PKG_INSTALL_LIST"
